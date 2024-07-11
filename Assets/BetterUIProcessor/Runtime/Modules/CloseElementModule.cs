fd633c78ff12ced1be381747a304b82ba6a242e1
Anatoly Brizhan <anatoly.brizhan@gmail.com>
2024-07-11 17:39:45 +0200

    Add CloseElementModule

---
+++ Assets/BetterUIProcessor/Runtime/Modules/CloseElementModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Better.UIProcessor.Runtime.Data;
+using Better.UIProcessor.Runtime.Interfaces;
+
+namespace Better.UIProcessor.Runtime.Modules
+{
+    [Serializable]
+    public class CloseElementModule : Module<CloseTransitionInfo>
+    {
+        protected override async Task<ProcessResult<IElement>> TryGetTransitionElement(UIProcessor processor, CloseTransitionInfo transitionInfo)
+        {
+            var result = await base.TryGetTransitionElement(processor, transitionInfo);
+            if (result.IsSuccessful)
+            {
+                return result;
+            }
+
+            return new ProcessResult<IElement>(default);
+        }
+    }
+}
